019a245835035a1a7d43b290200c0089363d1c3e
Igor Rangel <igordrangel@gmail.com>
2021-02-24 15:31:43 -0300

    commit

--- src/app/shared/services/imdb/imdb.service.ts
+++ src/app/shared/services/imdb/imdb.service.ts
@@ -10,17 +10,9 @@ export class ImdbService {
   public getPoster(name: string) {
     return new Observable<string>(observe => {
       this.http
-          .get('https://imdb8.p.rapidapi.com/title/auto-complete', {
-            headers: {
-              'x-rapidapi-key': '792ceedef1msh79e30f5c4b46eb7p15136djsn214c0784a07b',
-              'x-rapidapi-host': 'imdb8.p.rapidapi.com'
-            },
-            params: {
-              q: name
-            }
-          })
+          .get('https://imdb-api.com/en/API/Search/k_fwc4vynb/' + name)
           .subscribe((response: any) => {
-            observe.next(response?.d[0]?.i?.imageUrl ?? './assets/poster-default.jpg');
+            observe.next(response?.results[0]?.image ?? './assets/poster-default.jpg');
           }, error => observe.error(error));
     });
   }
